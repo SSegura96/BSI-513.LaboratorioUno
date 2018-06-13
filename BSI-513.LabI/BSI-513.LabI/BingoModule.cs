f8e45354c0a64443f6614222e3804277855940ea
Sergio Luis Segura Vidal <sergio31.96@outlook.com>
2018-06-13 10:36:00 -0600

    Initialize the project

---
+++ BSI-513.LabI/BSI-513.LabI/BingoModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BSI_513.LabI
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BingoModule" in both code and config file together.
+    public class BingoModule : IBingoModule
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
