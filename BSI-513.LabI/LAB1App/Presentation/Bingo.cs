5b30d2661a52d67b0de45fa1d04936f5dd40ba27
Sergio Luis Segura Vidal <sergio31.96@outlook.com>
2018-06-14 16:25:14 -0600

    Add a correct Structure

---
+++ BSI-513.LabI/LAB1App/Presentation/Bingo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1App.Presentation
+{
+    public class Bingo
+    {
+        public void Greeting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Menu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SelectTypeOfGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string ShowWinner()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BSI-513.LabI/LAB1App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  Algoritm
+             *  
+             *  1 - Ask to the user the type of game that he wants to play
+             *  2 - Fill the cardboard
+             *  3 - Ask the numeber of players
+             *  4 - Get the win pattern
+             *  5 - Show the winner
+             */
+
+
+        }
+    }
+}
