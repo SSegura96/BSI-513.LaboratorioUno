c4eaeeabffb1f9a0ecd3ce159631fa023f138400
Sofia96 <sofiajimenezm20@hotmail.com>
2018-06-22 16:31:54 -0600

    Initialize UnitTest Project

---
+++ Lab1App/Lab1Test/BingoActionsTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1UnitTest
+{
+    [TestClass]
+    public class BingoActionsTest
+    {
+
+        [TestMethod]
+        public void GetBingoColumnLetterTest()
+        {
+            WcfLab1.Domain.Actions.Bingo BingoAction = new WcfLab1.Domain.Actions.Bingo();
+            var Result = BingoAction.GetBingoColumnLetter(1);
+            Assert.IsTrue(Result.Equals("B"), "FalloB");
+
+            Result = BingoAction.GetBingoColumnLetter(30);
+            Assert.IsTrue(Result.Equals("I"), "Fallo I");
+        }
+
+    }
+}
